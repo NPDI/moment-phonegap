cfeee2cebaa81076b51d098cd9cdcd962d46e780
diegodifreitas <diegodifreitas@gmail.com>
2017-08-31 18:30:41 -0300

    New dependencies

--- moment-ddf/src/index.js
+++ moment-ddf/src/index.js
@@ -11,9 +11,24 @@ import './css/app.css';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {App} from './components/App';
 
+import promise from 'redux-promise'
+import multi from 'redux-multi'
+import thunk from 'redux-thunk'
+
+import { applyMiddleware, createStore } from 'redux'
+import { Provider } from 'react-redux'
+
+import reducers from './components/main/reducers'
+import {App} from './components/main/App';
+
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__
+&& window.__REDUX_DEVTOOLS_EXTENSION__()
+
+const store = applyMiddleware(multi, thunk, promise)(createStore)(reducers, devTools)
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
